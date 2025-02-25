e4f1a25a0da08642b487cae9125aa620c40f6207
Himanshu Daudia <himanshu.daudia@rdisrv.com>
2025-02-25 22:20:00 +0530

    add css
    add css

--- app/components/Hetmenu.jsx
+++ app/components/Hetmenu.jsx
@@ -65,7 +65,7 @@ const Hetmenu = () => {
           start: '0% 0%',
           end: '8% 8%',
           scrub: true,
-          once: true,
+          once: false,
         },
         borderRadius: '0vw 0vw 0px 0px',
         ease: 'power1.inOut',
